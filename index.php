82f925658f9b5ea7de9077109051a0dd895fd185
darkterminal <fscodexxx@yahoo.com>
2018-04-22 16:10:57 +0700

    Latihan mengetahui perbedaan echo/print/print_r

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 
-/**
- * Pan coba lu buka di browser, gimana hasilnya
- */
-echo 'Hello World!';
+/* 
+Tutorial 1 
+
+Belajar menggunakan ECHO & PRINT & PRINT_R
+	* Mengetahui perbedaan echo dan print serta print_r
+*/
+
+// menampilkan string
+print "Hello World <br>";
+
+// menampilkan string dengan echo
+echo "Hello World <br>";
+
+// print_r
+$smartphones = [
+	'Xiomi Redmi 2',
+	'Samsung Galaxy 8',
+	'Nokia 2000'
+];
+
+print_r($smartphones);

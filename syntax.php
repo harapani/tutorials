45c32eba4bcb86cd72d01531c419ec5ae1a56ef4
darkterminal <fscodexxx@yahoo.com>
2018-04-22 16:22:42 +0700

    Comment syntax in PHP

---
+++ syntax.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Comments in PHP
+	A comment in PHP code is a line that is not read/executed as part of the program. Its only purpose is to be read by someone who is looking at the code.
+
+Comments can be used to:
+
+	Let others understand what you are doing
+	Remind yourself of what you did - Most programmers have experienced coming back to their own work a year or two later and having to re-figure out what they did. Comments can remind you of what you were thinking when you wrote the code
+
+PHP supports several ways of commenting:
+ */
+
+// This is a single-line comment <--- inline comment
+
+# This is also a single-line comment <--- inline comment like C, C++
+
+/* 										---
+This is a multiple-lines comment block 	  | Block comments
+that spans over multiple 				  |
+lines 								    ---
+*/
